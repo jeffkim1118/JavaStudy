e579cc5161baec0879ec1a51a3daadc2c8e56049
Yoonsung Kim <64029918+jeffkim1118@users.noreply.github.com>
2024-03-10 14:07:58 -0400

    First java program commit

---
+++ src/FirstClass.java
@@ -0,0 +1,17 @@
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        boolean isAlien = false;
+        if (isAlien == false){
+            System.out.println("It is not an alien!");
+        }
+
+        int topScore = 100;
+        if (topScore >= 100){
+            System.out.println("You got the high score!");
+        }
+    }
+
+    
+}
