e54f68fb9060ceac48f84d6be5db4535d4ab9daf
Yoonsung Kim <64029918+jeffkim1118@users.noreply.github.com>
2024-03-18 17:08:06 -0400

    while loop challenge added

---
+++ src/WhileLoopChallenge.java
@@ -0,0 +1,14 @@
+public class WhileLoopChallenge {
+    public static void main(String[] args) {
+        int number = 4;
+        int finishNumber = 20;
+    }
+
+    public static boolean isEvenNumber(int number){
+        if((number % 2 == 0)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
