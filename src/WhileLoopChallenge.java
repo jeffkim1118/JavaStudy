a74dc2e60726990999105f36a354ba5a100a43ac
Yoonsung Kim <64029918+jeffkim1118@users.noreply.github.com>
2024-03-19 21:33:34 -0400

    while loop challenge edited

--- src/WhileLoopChallenge.java
+++ src/WhileLoopChallenge.java
@@ -2,6 +2,15 @@ public class WhileLoopChallenge {
     public static void main(String[] args) {
         int number = 4;
         int finishNumber = 20;
+
+        while(number <= finishNumber){
+            number++;
+            if(!isEvenNumber((number))){
+                continue;
+            }
+            System.out.println("Even number " + number);
+
+        }
     }
 
     public static boolean isEvenNumber(int number){
